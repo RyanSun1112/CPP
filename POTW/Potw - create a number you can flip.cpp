5b4f6a6d0aa0880d6377377b0f0c930ddaebbb8c
Jun Sun <mega.ryansun@gmail.com>
2020-06-19 11:39:02 -0400

    this is a long program

---
+++ POTW/Potw - create a number you can flip.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int num1 = 36336;
+    int num2 = 36333;
+    int num3 = 36366;
+    int num4 = 36363;
+    int num5 = 36636;
+    int num6 = 36633;
+    int num7 = 36663;
+    int num8 = 36666;
+    int num9 = 33633;
+    int num10 = 33636;
+    int num11 = 33663;
+    int num12 = 33666;
+    int num13 = 33366;
+    int num14 = 33363;
+    int num15 = 33333;
+    int num16 = 33336;
+    int num17 = 63663;
+    int num18 = 63666;
+    int num19 = 63636;
+    int num20 = 63633;
+    int num21 = 63363;
+    int num22 = 63366;
+    int num23 = 63336;
+    int num24 = 63333;
+    int num25 = 66333;
+    int num26 = 66336;
+    int num27 = 66363;
+    int num28 = 66366;
+    int num29 = 66636;
+    int num30 = 66633;
+    int num31 = 66663;
+    int num32 = 66666;
+
+    cout << num1 / 9 * 9 << endl << "if you divide 36336 by nine, then mutliply, it does not equal the same, so it is not divisble by 9 " << endl;
+    
+    cout << num2 / 9 * 9 << endl << "if you divide 36333 by nine, then mutliply, it is the same.so that means it is divisble" << endl;
+    
+    cout << num3 / 9 * 9 << endl << "if you divide 36366 by nine, then mutliply, it is not the same, so that means it is not divisble by 9" << endl;
+   
+    cout << num4 / 9 * 9 << endl << "if you divide 36363 by nine, then mutiply, it is not the same, so that means it is not divisble by 9" << endl;
+    
+    cout << num5 / 9 * 9 << endl << "if you divide 36636 by nine, then mutiply, it is not the same, so that means it is not divisble ny 9" << endl;
+    
+    cout << num6 / 9 * 9 << endl << "if you divide 36633 by nine, then mutiply, it is not the same so that means " << endl;
+    
+    cout << num7 / 9 * 9 << endl << "if you divide 36663 by nine, then mutiply, it will not be the same, so that means it is not divisble by 9" << endl;
+    
+    cout << num8 / 9 * 9 << endl << "if you divide 36666 by nine, then mutiply, it is the same. so that means it is divisble by 9" << endl;
+    
+    cout << num9 / 9 * 9 << endl << "if you divide 33633 by nine, then mutiply, it is the same. so that means it is divisble by 9" << endl;
+    
+    cout << num10 / 9 * 9 << endl << "if you divide 33636 by nine, then mutliply, it is not the same. therefore, it is not divisble by 9" << endl;
+    
+    cout << num11 / 9 * 9 << endl << "if you divide 33663 by nine, then multiply, it not the same. therefore it is not divisble by nine" << endl;
+    
+    cout << num12 / 9 * 9 << endl << "if you divide 33666 by nine, then multply, it is not the same. therefore, it is not divisble by 9." << endl;
+    
+    cout << num13 / 9 * 9 << endl << "if you divide 33366 by ninr, then multiply, it is not the same. therefore it is not divisble by 9" << endl;
+    
+    cout << num14 / 9 * 9 << endl << "if you divide 33363 by nine, then multiply, it is the same. therfore it is divisble by nine" << endl;
+    
+    cout << num15 / 9 * 9 << endl << "if you divide 33333 by nine, then multiply, it is not the same, so therefore it is not divisble by nine " << endl;
+    
+    cout << num16 / 9 * 9 << endl << "if you divide 33336 by nine, then mutliply, it is the same. therefore it is divisble by nine" << endl;
+    
+    cout << num17 / 9 * 9 << endl << "if you divide 63663 by nine, then mutliply, it is not the same. so therefore it is not divisble by nine" << endl;
+    
+    cout << num18 / 9 * 9 << endl << "if you divide 63666 by nine, then mutliply, it is the same. so therefore it is divisble by nine" << endl;
+    
+    cout << num19 / 9 * 9 << endl << "of you divide 63636 by nine, then multiply, it is not the same. therefore it is not divisble by 9" << endl;
+    
+    cout << num20 / 9 * 9 << endl << "if you divide 63633 by nine, then multiply, it is not the same. therefore it is not divisible by nine" << endl; 
+    
+    cout << num21 / 9 * 9 << endl << "if you divide 63363 by nine, then mutliply, it is not the same. therefore it is not divisble ny nine" << endl;
+    
+    cout << num22 / 9 * 9 << endl << "if you divide 63363 by nine, then mutiply, it is not the same. therefore it is not divisble by nine" << endl; 
+    
+    cout << num23 / 9 * 9 << endl <<" if you divide 63336 by nine, then mutliply, it is not the same. therefore it is not divisble by nine" << endl;
+    
+    cout << num24 / 9 * 9 << endl << "if you divide 63333 by nine, then mutliply, it is the same. therefore it is divisble by nine." << endl;
+    
+    cout << num25 / 9 * 9 << endl << "if you divide 66333 by nine, then mutliply, it is not the same. therefore it is not divisble by nine" << endl;
+    
+    cout << num26 / 9 * 9 << endl << "if you divide 66336 by nine, then multiply, it is not the same. therefore it is not divisble by nine" << endl;
+    
+    cout << num27 / 9 * 9 << endl << "if you divide 66363 by nine, then mutliply, it is not the same. so therefore it is not divisble by nine" << endl;
+    
+    cout << num28 / 9 * 9 << endl << "if you divide 66366 by nine, then nutliply,it is the same. therefore it is divisble by nine" << endl;
+    
+    cout << num29 / 9 * 9 << endl << "if you divide 66636 by nine, then mutliply, it is the same. therefore it is divisble by nine" << endl;
+    
+    cout << num30 / 9 * 9 << endl << "if you divide 66633 by nine, then mutliply, it is not the same. therefore it is not divisble by nine" << endl;
+    
+    cout << num31 / 9 * 9 << endl << "if tou divide 66663 by nine, then mutliply, it is the same. therefore it is divisble by nine" << endl;
+    
+    cout << num32 / 9 * 9 << endl << "if you divide 66666 by nine, then mutliply, it is not the same. therefore it is divisble by nine" << endl;
+    
+    cout << "There are 10 numbers that are divisble by nine.  33 336, 33 363, 33 633, 36 333, 63 333, 66 663, 66 636, 66 366,63 666, and 36 666." << endl;
+
+}
