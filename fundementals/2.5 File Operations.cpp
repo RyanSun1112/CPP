0492efb758308587f3bbbf9a28b2865226b44682
Jun Sun <mega.ryansun@gmail.com>
2020-07-05 09:49:24 -0400

    open this in the morning

---
+++ fundementals/2.5 File Operations.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ofstream fout ("open this in the morning");
+    string i = "wake up";
+    fout << i;
+ 
+    
+    return 0;
+}
