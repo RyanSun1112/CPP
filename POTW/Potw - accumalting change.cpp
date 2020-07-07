650f367604c135f0f520141f176109b258758326
Jun Sun <mega.ryansun@gmail.com>
2020-07-07 12:05:38 -0400

    stuff

---
+++ POTW/Potw - accumalting change.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int toon = 56;
+    cout << "toon equals = " << toon << endl;
+    double fquat = 3/15;
+    double floon = 5/15;
+    double fall = 15/15;
+    int all = 120;
+    double ftoon = 7/15;
+    int loon = 24;
+    int quat = 40;
+    float dquat = 10;
+    float dloon = 24;
+    float dtoon = 112;
+    int money;
+    ftoon = fall - (floon + fquat);
+    cout << "first we convert toonies in to a fraction" << endl;
+    cout << "fraction of toonies equals 7/15 because in order to get it, we subtract the fraction of the whole amount by the fraction of the loonies and quaters, you get the fraction of toonies. " << endl;
+    all = 56 / 7 * 15;
+    cout << "in total, there are 120 because since we know that the fraction of toonies is equal to 7/15, if we divide it by 7 then mutliply by 15, we will get the total amount of coins" << endl; 
+    loon = floon * 120;
+    quat = fquat * 120;
+    cout << "when the fraction of loon is mutliply by 120, you get how many loonies there are = 24" << endl;
+    cout << " when the fraction of quarters is mutluplyed by 120, you get how many quarters there are in total = 40" << endl;
+    dquat = 0.25 * 40; 
+    dloon = 1 * 24;
+    dtoon = 2 * 56;
+    cout << " dollar of quarter equals 10 dollars" << endl;
+    cout << " dollar of loonies equals 24 dollars" << endl;
+    cout << " dollar of toonies equals 112 dollars" << endl;
+    money = dquat + dloon + dtoon;
+    cout << " total dollars saved equals 146 dollars" << endl;
+    
+    return 0;
+}
--- fundementals/2.5 File Operations.cpp
+++ fundementals/2.5 File Operations.cpp
@@ -6,6 +6,20 @@ int main() {
     ofstream fout ("open this in the morning");
     string i = "wake up";
     fout << i;
+
+    ifstream fin ("file.out");
+    string str;
+    fin >> str;
+    cout << str;
+    fin >> str;
+    cout << str;
+
+
+    // ifstream cin ("fundementals/file.in");
+    // int x , y , z;
+    // cin >> x;
+    // cin >> y;
+    // cin >> z;
  
     
     return 0;
